d1624f25be04e7107fd7ba368537d4276063f929
roeeharush <roeeharush31@gmail.com>
2026-05-07 19:02:03 +0300

    -added Plant class

--- src/ecosystem/entities/plants/Plant.java
+++ src/ecosystem/entities/plants/Plant.java
@@ -1,4 +1,5 @@
 package ecosystem.entities.plants;
+import ecosystem.core.Environment;
 import ecosystem.core.Position;
 import ecosystem.entities.LivingEntity;
 import ecosystem.interfaces.Consumable;
@@ -7,15 +8,14 @@ import ecosystem.interfaces.Reproducible;
 
 public abstract class Plant extends LivingEntity implements Consumable, Reproducible, EdibleByHerbivore {
     private double growthRate;
-    private int reproductionChance;
+    private double reproductionChance;
 
-    public Plant(Position position, char symbol, boolean alive, int energy, int maxEnergy, double growthRate, int reproductionChance) {
+    public Plant(Position position, char symbol, boolean alive, double energy, double maxEnergy, double growthRate, double reproductionChance) {
         super(position, symbol, alive, energy, maxEnergy);
         if(!setGrowRate(growthRate))
-            this.growthRate = 1;
+            this.growthRate = 1.0;
         if(!setReproductionChance(reproductionChance))
-            this.reproductionChance = 10;
-
+            this.reproductionChance = 0.1;
     }
 
     public boolean setGrowRate(double growthRate){
@@ -26,12 +26,55 @@ public abstract class Plant extends LivingEntity implements Consumable, Reproduc
         return false;
     }
 
-    public boolean setReproductionChance(int reproductionChance){
-        if(reproductionChance <= 100 && reproductionChance >= 0)
+    public boolean setReproductionChance(double reproductionChance){
+        if(reproductionChance <= 1.0 && reproductionChance >= 0)
         {
             this.reproductionChance = reproductionChance;
             return true;
         }
         return false;
     }
+
+    @Override
+    public boolean act(Environment env){
+        if(!super.act(env))
+            return false;
+        setEnergy(getEnergy() + growthRate + 2.0);
+        reproduce(env);
+        return true;
+    }
+    @Override
+    public abstract boolean reproduce(Environment env);
+
+
+    @Override
+    public double getNutritionValue(){
+        return this.getEnergy();
+
+    }
+
+    @Override
+    public boolean onConsumed(){
+        return this.setAlive(false);
+    }
+
+    /*
+    @Override
+    public String toString(){
+
+    }
+*/
+    @Override
+    public boolean equals(Object o){
+        if(this == o)
+            return true;
+        if(o instanceof Plant other){
+            if(!super.equals(o))
+                return false;
+            return Double.compare(this.growthRate,other.growthRate) ==0
+                    && Double.compare(this.reproductionChance,other.reproductionChance)==0;
+        }
+        return false;
+    }
+
 }
