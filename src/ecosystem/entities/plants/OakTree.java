a753ba156a33e54edc7302fe06862e84b6a74467
roeeharush <roeeharush31@gmail.com>
2026-05-07 19:03:19 +0300

    -added OakTree constructor

--- src/ecosystem/entities/plants/OakTree.java
+++ src/ecosystem/entities/plants/OakTree.java
@@ -1,4 +1,21 @@
 package ecosystem.entities.plants;
 
-public class OakTree {
+import ecosystem.core.Environment;
+import ecosystem.core.Position;
+
+public class OakTree extends Plant {
+
+    public OakTree(Position position){
+        super(position,'T',true,80.0,120.0,2.0,0.05);
+    }
+
+    @Override
+    public boolean reproduce(Environment env){
+        double chance = Math.random();
+        if(chance <= 0.05 && chance > 0.0){
+
+        }
+        return false;
+    }
+
 }
