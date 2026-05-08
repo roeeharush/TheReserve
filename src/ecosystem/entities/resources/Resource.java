2ffee59f9a463e7d7993e23c772b42c2a2e9072b
roeeharush <roeeharush31@gmail.com>
2026-05-08 12:07:13 +0300

    -added class Resource

--- src/ecosystem/entities/resources/Resource.java
+++ src/ecosystem/entities/resources/Resource.java
@@ -1,4 +1,26 @@
 package ecosystem.entities.resources;
 
-public class Resource {
+import ecosystem.core.Position;
+import ecosystem.entities.StaticEntity;
+
+public abstract class Resource extends StaticEntity {
+
+    public Resource(Position position, char symbol, boolean alive){
+        super(position, symbol, alive);
+    }
+
+
+    @Override
+    public boolean equals(Object o){
+        if(this == o)
+            return true;
+        if(o instanceof Resource other)
+            return super.equals(o);
+        return false;
+    }
+
+    @Override
+    public String toString(){
+        return "Entity Type: " + getSymbol() + " Position: " + getPosition().toString() +" Amount of Energy: " + "0.0 " + "is exist: " + isAlive();
+    }
 }
