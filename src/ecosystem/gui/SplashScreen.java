b128bb2021e24bdd35ee05a7291707dc0c742368
Renanazulay <renanaaz16@gmail.com>
2026-05-24 13:08:54 +0300

    -Added SplashScreen

--- src/ecosystem/gui/SplashScreen.java
+++ src/ecosystem/gui/SplashScreen.java
@@ -1,11 +1,42 @@
 package ecosystem.gui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SplashScreen extends JFrame {
+    private ImageIcon splashImage;
+    private JButton startButton;
+    private start act;
+
 
     public SplashScreen(){
+        super("WELCOME TO OUR GAME!");
+        setLayout(  new BorderLayout());
+        setResizable(false);
+        initComponents();
+        pack();
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
 
+    }
+
+    private void initComponents() {
+        splashImage = ImageLoader.getImage("start");
+        JLabel imageLabel = new JLabel(splashImage);
+        add(imageLabel, BorderLayout.CENTER);
+
+        startButton = new JButton("START");
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                new start();
+            }
+        });
+        add(startButton, BorderLayout.SOUTH);
 
     }
 }
